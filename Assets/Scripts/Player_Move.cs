68f171ea2a3a2740fb5cc4c7eb16c5aa332943f2
김남우 <rlaskadn02@naver.com>
2025-05-22 23:11:25 +0900

    Update

--- Assets/Scripts/Player_Move.cs
+++ Assets/Scripts/Player_Move.cs
@@ -1,10 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mono.Cecil.Cil;
+using Unity.Mathematics;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Player_Move : MonoBehaviour
 {
+    private bool canPlay = false;
+
+    int HP;
+
     public float jumpPower;
     public float speed;
-    
+
     float curSpeed;
     float h;
 
@@ -16,6 +25,7 @@ public class Player_Move : MonoBehaviour
 
     public AnimationClip[] animClip;
     public Animation anim;
+
     Rigidbody2D rbody;
     Animator animator;
 
@@ -23,33 +33,71 @@ public class Player_Move : MonoBehaviour
     public Vector2 attackOffset;
     public Vector2 attackSize;
 
+    HashSet<int> blockedStates;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake(){
+    void Awake()
+    {
         rbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animation>();
         animator = GetComponent<Animator>();
+
+        blockedStates = new HashSet<int>
+        {
+            Animator.StringToHash("Player_Attack1"),
+            Animator.StringToHash("Player_Attack2"),
+            Animator.StringToHash("Player_Attack3"),
+            Animator.StringToHash("Player_Death")
+        };
     }
 
-    void Start(){
+    void Start()
+    {
+        StartCoroutine(EnableInputAfterDelay(0.3f));
         attackOffset = new Vector2(1.5f, -0.45f);
         attackSize = new Vector2(1.2f, 2f);
         jumpAble = true;
         jumping = false;
+        HP = 100;
     }
 
     // Update is called once per frame
-    void Update(){
-        Move();
-        Motion();
+    void Update()
+    {
+        if (!canPlay)
+        {
+            return;
+        }
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        int currentHash = stateInfo.shortNameHash;
+
+        if (!blockedStates.Contains(currentHash))
+        {
+            Move();
+            Motion();
+            Skill();
+        }
+
+        if (HP <= 0)
+        {
+            Debug.Log("Player_Dead");
+            animator.Play("Player_Dead");
+        }
     }
 
-    void Move(){
+    void Move()
+    {
         h = Input.GetAxisRaw("Horizontal");
-        if(Input.GetKey(runKey)){
-            curSpeed = speed*1.5f;
-        }else{
+        if (Input.GetKey(runKey))
+        {
+            curSpeed = speed * 1.5f;
+        }
+        else
+        {
             curSpeed = speed;
-        }if(Input.GetKeyDown(jumpKey) && jumpAble == true){
+        }
+        if (Input.GetKeyDown(jumpKey) && jumpAble == true)
+        {
             jumpAble = false;
             jumping = true;
             rbody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
@@ -61,29 +109,42 @@ public class Player_Move : MonoBehaviour
         transform.position = curPos + nextPos;
     }
 
-    void Motion(){
-        if(h > 0){
+    void Motion()
+    {
+        if (h > 0)
+        {
             attackOffset = new Vector2(1.5f, -0.45f);
             transform.localScale = new Vector3(1, 1, 1);
-        }else if(h < 0){
+        }
+        else if (h < 0)
+        {
             attackOffset = new Vector2(-1.5f, -0.45f);
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        if(jumping == true){
+        if (jumping == true)
+        {
             animator.Play("Player_Jump");
-        }else if(h != 0){
-            if(curSpeed>speed){
+        }
+        else if (h != 0)
+        {
+            if (curSpeed > speed)
+            {
                 animator.Play("Player_Run");
-            }else{
+            }
+            else
+            {
                 animator.Play("Player_Walk");
             }
-        }else{
+        }
+        else
+        {
             animator.Play("Player_Idle");
         }
     }
 
-    void Attack(){
+    void Skill()
+    {
         Vector2 attackPos = (Vector2)transform.position + attackOffset;
         Collider2D[] hits = Physics2D.OverlapBoxAll(attackPos, attackSize, 0f, enemyLayer);
 
@@ -92,19 +153,45 @@ public class Player_Move : MonoBehaviour
             Debug.Log("적 감지됨: " + hit.name);
             hit.GetComponent<Enemy>()?.TakeDamage(1);
         }*/
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.Play("Player_Attack1");
+            //StartCoroutine(Attack1(0.05f));
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.Play("Player_Attack2");
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.Play("Player_Attack3");
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.Play("Player_Defend");
+        }
     }
 
-    void OnDrawGizmosSelected(){
+    void OnDrawGizmosSelected()
+    {
         Gizmos.color = Color.red;
         Vector2 attackPos = (Vector2)transform.position + attackOffset;
         Gizmos.DrawWireCube(attackPos, attackSize);
     }
 
-
-    void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.CompareTag("Ground")){
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
             jumping = false;
             jumpAble = true;
         }
+    }
+
+    IEnumerator EnableInputAfterDelay(float cool)
+    {
+        yield return new WaitForSeconds(cool);
+        canPlay = true;
+        Debug.Log("플레이 가능");
     }
 }
